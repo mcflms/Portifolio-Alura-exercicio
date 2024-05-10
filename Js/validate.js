74fd43659266859b8c67dae2ddd29fe135d9d63c
Marcio das Chagas <mcflms@gmail.com>
2024-05-09 23:37:42 -0300

    atualização do codigo, inclusão link com repositorio

--- Js/validate.js
+++ Js/validate.js
@@ -7,26 +7,3 @@ camposDoFormulario.forEach((campo) => {
 
 })
 
-function verificaCampo(campo){
-
-}
-
-const tiposDEErros = [
-    'valueMissing',
-    'typeMismatch',    
-    'tooShort',
-    
-]
-
-const mensagens = {
-    nome: {
-        valueMissing: "O campo de nome não pode estar vazio.",
-        patternMismatch: "Por favor, preencha um nome válido.",
-        tooShort: "Por favor, preencha um nome válido."
-    },
-    email: {
-        valueMissing: "O campo de e-mail não pode estar vazio.",
-        typeMismatch: "Por favor, preencha um email válido.",
-        tooShort: "Por favor, preencha um e-mail válido."
-    }
-}
